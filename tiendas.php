34d49a23d73ae1b3680de23f45c4ca56a8f35005
Maylin-RC <katherine.ramirez2027@adoc.superate.org>
2026-07-18 14:07:56 -0600

    Actualizo tiendas y mejoras de diseño

--- tiendas.php
+++ tiendas.php
@@ -16,6 +16,7 @@ $resultado = $conn->query($sql);
     <title>Tiendas de Surf</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/tiendas.css">
     <link rel="stylesheet" href="css/navbar.css">
